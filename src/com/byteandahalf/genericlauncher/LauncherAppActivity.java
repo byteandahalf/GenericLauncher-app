def1574cdb3f812c175fe70a48c0787475a1086b
Byteandahalf <byteandahalf131@gmail.com>
2014-06-19 00:08:54 -0400

    Build GenericLauncher with this

---
+++ src/com/byteandahalf/genericlauncher/LauncherAppActivity.java
@@ -0,0 +1,9 @@
+package com.byteandahalf.genericlauncher;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.byteandahalf.genericlauncher.ui.LauncherActivity;
+
+public class LauncherAppActivity extends LauncherActivity {
+}
